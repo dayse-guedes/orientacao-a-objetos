1c64274cad5ef9d844fbbeebf69343e920ff0743
daydocx <contato.dayseguedes@gmail.com>
2023-08-14 13:53:33 -0300

    :boom: fix: Revertendo mudanças ineficientes

--- telasom/Musica.cs
+++ telasom/Musica.cs
@@ -1,16 +1,18 @@
 ﻿ class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
+    public string Nome { get; set; } 
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
  public bool Disponivel { get; set; } //get leitura, set escrita
 
-    
+    public string NomeCompleto { get; set; } //prob atalho para criar uma propriedade
+
+
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"Nome: {nome}"); 
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duração: {duracao}");
+        Console.WriteLine($"Nome: {Nome}"); 
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
         if(Disponivel)
         {
             Console.WriteLine("Disponível no plano");
