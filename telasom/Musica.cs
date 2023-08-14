f4dbe10b113546c72681c141d781e6fa1b9a863c
daydocx <contato.dayseguedes@gmail.com>
2023-08-14 13:34:28 -0300

    :recycle: refactor: Refatorando a propriedade Disponivel com get, set

--- telasom/Musica.cs
+++ telasom/Musica.cs
@@ -3,26 +3,15 @@
     public string nome;
     public string artista;
     public int duracao;
-    private bool disponivel;
+ public bool Disponivel { get; set; } //get leitura, set escrita
 
-    public void EscreveDisponivel(bool value)
-    {
-        disponivel = value;
-    }
-
-    //Preciso proteger o atributo disponivel, preciso criar dois medotos portanto, um para leitura do disponivel e outro para escrita.
-    //Para tanto a função de escrever recebe um bool, um argumento, um valor de disponivel e no entanto essa escrita é privada, logo não pode ser alterada facilmente
-    //Em contrapartidada, a leitura é publica e retorna o disponivel no console.
-    public bool LeDisponivel()
-    {
-        return disponivel;
-    }
+    
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome: {nome}"); 
         Console.WriteLine($"Artista: {artista}");
         Console.WriteLine($"Duração: {duracao}");
-        if(disponivel)
+        if(Disponivel)
         {
             Console.WriteLine("Disponível no plano");
         } else 
