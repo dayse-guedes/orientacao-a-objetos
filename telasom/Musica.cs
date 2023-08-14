3a741903f05806f93344412a861e6fa3dfaf8073
daydocx <contato.dayseguedes@gmail.com>
2023-08-14 12:47:48 -0300

    :sparkles: feat: metodo de leitura e escrita de bool

--- telasom/Musica.cs
+++ telasom/Musica.cs
@@ -3,9 +3,20 @@
     public string nome;
     public string artista;
     public int duracao;
-    public bool disponivel;
+    private bool disponivel;
 
+    public void EscreveDisponivel(bool value)
+    {
+        disponivel = value;
+    }
 
+    //Preciso proteger o atributo disponivel, preciso criar dois medotos portanto, um para leitura do disponivel e outro para escrita.
+    //Para tanto a função de escrever recebe um bool, um argumento, um valor de disponivel e no entanto essa escrita é privada, logo não pode ser alterada facilmente
+    //Em contrapartidada, a leitura é publica e retorna o disponivel no console.
+    public bool LeDisponivel()
+    {
+        return disponivel;
+    }
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome: {nome}"); 
--- telasom/Programa.cs
+++ telasom/Programa.cs
@@ -2,13 +2,15 @@
 musica1.nome = "Divino Maravilhoso";
 musica1.artista = "Gal Costa";
 musica1.duracao = 273;
-musica1.disponivel = true;
+musica1.EscreveDisponivel(true);
+Console.WriteLine(musica1.LeDisponivel());
 
 Musica musica2 = new Musica();
 musica2.nome = "Azul";
 musica2.artista = "Chico Buarque";
 musica2.duracao = 373;
-musica2.disponivel = false;
+musica2.EscreveDisponivel(false);
+Console.WriteLine(musica2.LeDisponivel());
 
 musica1.ExibirFichaTecnica();
 
