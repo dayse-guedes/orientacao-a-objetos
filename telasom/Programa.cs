e9365233084885c21a311bde4008290ba994a0a5
daydocx <contato.dayseguedes@gmail.com>
2023-08-18 19:41:50 -0300

    :sparkles: feat: Criação da classe banda

--- telasom/Programa.cs
+++ telasom/Programa.cs
@@ -3,13 +3,18 @@ AlbumDoCaetano.Nome = "Transa";
 
 Musica musica1 = new Musica();
 musica1.Nome = "You Don't Know Me";
+musica1.Duracao = 250;
 
 
 
 Musica musica2 = new Musica();
 musica2.Nome = "Nine Out of Ten";
+musica2.Duracao = 345;
 
 AlbumDoCaetano.AdicionarMusica(musica1);
 AlbumDoCaetano.AdicionarMusica(musica2);
 
-AlbumDoCaetano.ExibaMusicasDoAlbum();
+Banda caetano = new Banda();
+caetano.Nome = "Caetano Veloso";
+caetano.AdicionarAlbum(AlbumDoCaetano);
+caetano.ExibirDiscografia();
