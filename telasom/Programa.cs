2adceaaed89f1961e18bf43fefd7faae2f903726
daydocx <contato.dayseguedes@gmail.com>
2023-08-21 11:12:28 -0300

    :sparkles: feat: criação de um construtor

--- telasom/Programa.cs
+++ telasom/Programa.cs
@@ -1,20 +1,23 @@
-﻿Album AlbumDoCaetano = new Album();
-AlbumDoCaetano.Nome = "Transa";
+﻿Banda queen = new Banda();
+queen.Nome = "Queen";
 
-Musica musica1 = new Musica();
+Album AlbumDoQueen = new Album();
+AlbumDoQueen.Nome = "The nigth of opera";
+
+Musica musica1 = new Musica(queen);
 musica1.Nome = "You Don't Know Me";
 musica1.Duracao = 250;
 
 
 
-Musica musica2 = new Musica();
+Musica musica2 = new Musica(queen);
 musica2.Nome = "Nine Out of Ten";
 musica2.Duracao = 345;
 
-AlbumDoCaetano.AdicionarMusica(musica1);
-AlbumDoCaetano.AdicionarMusica(musica2);
+AlbumDoQueen.AdicionarMusica(musica1);
+AlbumDoQueen.AdicionarMusica(musica2);
 
-Banda caetano = new Banda();
-caetano.Nome = "Caetano Veloso";
-caetano.AdicionarAlbum(AlbumDoCaetano);
-caetano.ExibirDiscografia();
+AlbumDoQueen.ExibaMusicasDoAlbum();
+
+
+
