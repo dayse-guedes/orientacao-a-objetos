c767abf6a2ccf75556481029809871619736229d
daydocx <contato.dayseguedes@gmail.com>
2023-08-21 14:29:24 -0300

    criação da classe e construtor epsodio

--- telasom/Programa.cs
+++ telasom/Programa.cs
@@ -1,35 +1,7 @@
-﻿Banda queen = new Banda("Queen");
-
-
-Album AlbumDoQueen = new Album("The nigth of opera");
-
-Musica musica1 = new Musica(queen,"You Don't Know Me")
-{
-     Duracao = 250,
-     Disponivel = true,
-};
-
-
-Musica musica2 = new Musica(queen, "Nine Out of Ten")
-{ //Inicializadores de propriedades
-    Duracao = 375,
-    Disponivel = false,
-
-
-};
-
-AlbumDoQueen.AdicionarMusica(musica1);
-AlbumDoQueen.AdicionarMusica(musica2);
-queen.AdicionarAlbum(AlbumDoQueen);
-
-
-
-
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
-AlbumDoQueen.ExibaMusicasDoAlbum();
-queen.ExibirDiscografia();
-
+﻿Epsodio ep1 = new(1, "Técnicas de facilitação", 45);
+ep1.AdicionarCovidados("Dayse Guedes");
+ep1.AdicionarCovidados("Felipe Miosse");
+Console.WriteLine(ep1.Resumo);
 
 
 
